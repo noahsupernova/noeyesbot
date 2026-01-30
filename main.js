b3bf8ccb211b2886d5f510e432e8c26d58815305
noahsupernova <noahpark42@gmail.com>
2026-01-30 10:31:10 +0900

    init portfolio

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const btn = document.getElementById('btn');
+const btn = document.getElementById("btn");
 
-btn.addEventListener('click', () => {
-  alert('깃허브 테스트 성공 🔥');
+btn.addEventListener("click", () => {
+  alert("깃허브 테스트 성공 🔥");
 });
